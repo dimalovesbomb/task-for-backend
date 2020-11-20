174ddf8de472aede1713c3e523284d969b882e50
Dmitrii Kupriunin <kupriunin@gmail.com>
2020-11-20 13:59:12 +0300

    setted random meme side

--- src/components/home.js
+++ src/components/home.js
@@ -1,7 +1,7 @@
 import { Divider, List, ListItem, ListItemIcon, ListItemText, makeStyles } from '@material-ui/core';
 import DashboardIcon from '@material-ui/icons/Dashboard';
 import BarChartIcon from '@material-ui/icons/BarChart';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import  { Link } from 'react-router-dom';
 
 const useStyles = makeStyles( theme => ({
@@ -14,18 +14,43 @@ const useStyles = makeStyles( theme => ({
     },
     randomPic: {
         position: 'absolute',
+        display: 'flex',
+        flexDirection: 'column',
         top: 10,
         left: 10,
-        width: 200
+        width: 400,
+        height: 400
+    },
+    'randomPic img': {
+        width: '25%',
+        height: '25%'
+    },
+    randomLink: {
+        fontSize: 12
     }
 }))
 
 export const Home = props => {
+    const [meme, setMeme] = useState({});
+
     const classes = useStyles();
+
+    useEffect( () => {
+        fetch('https://meme-api.herokuapp.com/gimme')
+            .then(res => res.json())
+            .then(res => setMeme(res))
+            .catch(error => console.error(error));
+    }, []);
+
+    console.log(meme);
 
     return (
         <div className={classes.home}>
-            <img className={classes.randomPic} src={"src/cat.jpg"}></img>
+            <div className={classes.randomPic}>
+                <img src={meme.url} alt={meme.title} />
+                <a className={classes.randomLink} color="secondary" href={meme.postLink} target="_blank">To this post</a>
+            </div>
+
             <List>
                 <Divider />
                 <Link to="/configuration">
